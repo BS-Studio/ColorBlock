e6544ac89dce6c36a28593069c1b90f3aa89ecf9
Bill Zhao <rizhao@ris-imac.local>
2018-08-04 12:44:05 -0400

    Initial Commit

---
+++ OutfitBlock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OutfitBlock
+//
+//  Created by Ri Zhao on 2018-08-04.
+//  Copyright © 2018 Ri Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
